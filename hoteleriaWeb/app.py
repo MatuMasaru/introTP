22ca5c50639143b4f973b98bd1e19dfd722a5f0a
tvandevelde17 <tvandevelde@fi.uba.ar>
2024-11-11 18:59:37 -0300

    -Ruteo y conexion de todas las vistas. -Termine de linkear los {{url_for}} para los archivos css, js y los img_src. -Adaptacion a Flask de la vista 404.html -En info_hotel, cree una funcion para que traiga la informacion del hotel seleccionado (dentro de un ciclo for por si se necesita mostrar la informacion de todos los hoteles). -Idem en habitaciones.html, con la informacion de las habitaciones disponibles a reservar.

--- hoteleriaWeb/app.py
+++ hoteleriaWeb/app.py
@@ -8,14 +8,14 @@ def index():
     hotels = [
         {
             "id": 1,
-            "image_url": "static/assets/hotel-bariloche.jpg",
+            "image_url": "static/assets/hotel-buenos-aire.jpg",
             "name": "Buenos Aires",
             "description": "Ubicado en el corazón de la vibrante capital, nuestro hotel ofrece un oasis de tranquilidad en medio del bullicio urbano. Ideal para viajeros de negocios y turistas por igual.",
             "link": "#inicio",
         },
         {
             "id": 2,
-            "image_url": "static/assets/hotel-bariloche.jpg",
+            "image_url": "static/assets/hotel-mar-del-plata.jpg",
             "name": "Mar del Plata",
             "description": "Perfecto para quienes buscan la combinación ideal de playa y ciudad. Disfrute del sol, la arena y las actividades culturales que esta hermosa ciudad costera tiene para ofrecer.",
             "link": "#inicio",
@@ -33,12 +33,105 @@ def index():
 
 @app.route("/mi_reserva")
 def mi_reserva():
+    return render_template('login_mis_reservas.html')
+
+@app.route("/informacion_reserva")
+def informacion_reserva():
     return render_template('ver_mis_reservas.html')
 
+@app.route("/hoteles/<id_hotel>")
+def hoteles(id_hotel):
+    id_hotel = int(id_hotel)
+    hotels = [
+        {
+            "id": 1,
+            "image_url": "static/assets/hotel-buenos-aire.jpg",
+            "name": "Buenos Aires",
+            "ubication": "Avenida de Mayo 123",
+            "description": "Ubicado en el corazón de la vibrante capital, nuestro hotel ofrece un oasis de tranquilidad en medio del bullicio urbano. Ideal para viajeros de negocios y turistas por igual.",
+            "link": "#inicio",
+            "services": ["Gimnasio", "Spa", "Sauna", "Mesas de Pool", "Piscina"]
+        },
+        {
+            "id": 2,
+            "image_url": "static/assets/hotel-mar-del-plata.jpg",
+            "name": "Mar del Plata",
+            "ubication": "Av. de Los Trabajadores 3100",
+            "description": "Perfecto para quienes buscan la combinación ideal de playa y ciudad. Disfrute del sol, la arena y las actividades culturales que esta hermosa ciudad costera tiene para ofrecer.",
+            "link": "#inicio",
+            "services": ["Gimnasio", "Spa", "Piscina"]
+        },
+        {
+            "id": 3,
+            "image_url": "static/assets/hotel-bariloche.jpg",
+            "name": "Bariloche",
+            "ubication": "Cerro Catedral 1450",
+            "description": "Situado en el pintoresco escenario de la Patagonia, este hotel es ideal para los amantes de la naturaleza y los deportes de invierno. Disfrute de vistas impresionantes, actividades al aire libre y la hospitalidad cálida de siempre.",
+            "link": "#inicio",
+            "services": ["Gimnasio","Sauna", "Mesas de Pool", "Piscina"]
+        },
+    ]
 
-@app.route("/hoteles")
-def hoteles():
-    return render_template('info_hotel.html')
+    if id_hotel == 0:
+        return render_template('info_hotel.html', params = hotels)
+    params = []
+    params.append(hotels[id_hotel])
+
+    return render_template('info_hotel.html', params = params)
+
+@app.route("/habitaciones")
+def habitaciones():
+    hotels = [
+        {
+            "id": 1,
+            "image_url": "static/assets/hotel-buenos-aire.jpg",
+            "name": "Buenos Aires",
+            "ubication": "Avenida de Mayo 123",
+            "description": "Ubicado en el corazón de la vibrante capital, nuestro hotel ofrece un oasis de tranquilidad en medio del bullicio urbano. Ideal para viajeros de negocios y turistas por igual.",
+            "link": "#inicio",
+            "services": ["Gimnasio", "Spa", "Sauna", "Mesas de Pool", "Piscina"]
+        },
+        {
+            "id": 2,
+            "image_url": "static/assets/hotel-mar-del-plata.jpg",
+            "name": "Mar del Plata",
+            "ubication": "Av. de Los Trabajadores 3100",
+            "description": "Perfecto para quienes buscan la combinación ideal de playa y ciudad. Disfrute del sol, la arena y las actividades culturales que esta hermosa ciudad costera tiene para ofrecer.",
+            "link": "#inicio",
+            "services": ["Gimnasio", "Spa", "Piscina"]
+        },
+        {
+            "id": 3,
+            "image_url": "static/assets/hotel-bariloche.jpg",
+            "name": "Bariloche",
+            "ubication": "Cerro Catedral 1450",
+            "description": "Situado en el pintoresco escenario de la Patagonia, este hotel es ideal para los amantes de la naturaleza y los deportes de invierno. Disfrute de vistas impresionantes, actividades al aire libre y la hospitalidad cálida de siempre.",
+            "link": "#inicio",
+            "services": ["Gimnasio","Sauna", "Mesas de Pool", "Piscina"]
+        },
+    ]
+    rooms = [
+        {   
+            "id_room": 0,
+            "id_hotel": 0,
+            "number": 150,
+            "persons": 4,
+            "price": 40000
+        },
+        {   
+            "id_room": 1,
+            "id_hotel": 2,
+            "number": 300,
+            "persons": 4,
+            "price": 40000
+        }   
+
+    ]
+    return render_template('habitaciones.html', rooms = rooms, hotels = hotels)
+
+@app.route("/reservar/<id_room>")
+def reservar(id_room):
+    return render_template("reservas.html")
 
 @app.errorhandler(404)
 def page_notfound(e):
