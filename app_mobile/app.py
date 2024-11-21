6db51141e68cc9f874c021a10c5f94479ddabda9
arturo,sanchez <asanchezc@fi.uba.ar>
2024-11-20 22:40:55 -0300

    se adapta la app.py de la mobile a la api de general id[6][1] pude: -ver detalles de la reserva -cancelar reserva ------proximamente -agregar y/o cancelar servicios

--- app_mobile/app.py
+++ app_mobile/app.py
@@ -4,39 +4,43 @@ from kivy.uix.screenmanager import ScreenManager, Screen, SwapTransition
 from kivymd.app import MDApp
 import requests
 
-
-# pantallas
+# Definir las pantallas
 class MainScreen(Screen):
     pass
 
 class DetailsScreen(Screen):
-    def get_reservation_details(self, code):
-        response = requests.get(f'http://127.0.0.1:3648/api/reserva/detalles/{code}')
+    def get_reservation_details(self, code , name):
+        response = requests.get(f'http://127.0.0.1:3648/api/reserva/{code}/{name}')
         if response.status_code == 200:
             datas = response.json()
             data=datas[0]
             self.ids.details_label.text =(
-                f"Nombre: {data['nombre']}\n"
-                f"Correo: {data['correo']}\n"
+                f"Cliente: {data['cliente_apellido']}\n"
+                f"Estado de la reserva: {data['estado']}\n"
                 f"Llegada: {data['llegada']}\n"
                 f"Salida: {data['salida']}\n"
-                f"Estado: {data['estado']}\n"
+                f"USD: {data['precio']}\n"
+                f"ID habitacion: {data['id_habitacion']}\n"
+                f"fecha de cancelacion: {data['fecha_cancelacion']}\n"
+                f"codigo de reserva: {data['id']}\n"
             )
         else:
             self.ids.details_label.text = "Reserva no encontrada."
 
 class CancelScreen(Screen):
     def cancel_reservation(self, code, name):
-        url = f"http://127.0.0.1:3648/api/reserva/cancelar/{code}/{name}"
-        response = requests.post(url)
+        url = f"http://127.0.0.1:3648/api/reserva/{code}/{name}"
+        response = requests.put(url)
         if response.status_code == 200:
             self.ids.cancel_label.text = "Reserva cancelada exitosamente."
         elif response.status_code == 400:
             self.ids.cancel_label.text = "Su reserva ya se encuentra cancelada"
+        elif response.status_code ==404:
+            self.ids.cancel_label.text = "su codigo de reserva no esta registrada"
         else:
             self.ids.cancel_label.text = "Error al cancelar la reserva."
 
-# gestor de pantallas
+# Crear el gestor de pantallas
 class ReservationApp(MDApp):
     def build(self):
         return Builder.load_file('desig.kv')
